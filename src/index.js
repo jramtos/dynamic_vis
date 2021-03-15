cd3f0dc4498942a2a88134883d487c7ebc645749
Jesica Ramirez <jramireztoscano@uchicago.edu>
2021-03-14 18:48:00 -0600

    steps pending, heatmap aesthetics finished

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import { nest } from 'd3-collection';
 import { line } from 'd3-shape';
 import { scaleLinear, scaleOrdinal, scaleBand, scaleSequential } from 'd3-scale';
 import { axisBottom, axisLeft } from 'd3-axis';
-import { extent, schemeSet2, format, interpolatePuBuGn } from 'd3';
+import { extent, schemeSet2, format, interpolatePuBuGn, range } from 'd3';
 import { transition } from 'd3-transition';
 import "intersection-observer";
 import scrollama from "scrollama";
+import { legendColor } from 'd3-svg-legend'
 
 //Helper functions
 function unique(data, key) {
@@ -66,7 +67,16 @@ const mental_illness_dic = {
     2: 'Serious Mental Illness'
 }
 
+const emotional_dic = {
+    1: 'None of the time',
+    2: 'A Little of the time',
+    3: 'Some of the time',
+    4: 'Most of the time',
+    5: 'All the time'
+}
+
 const mental_ill = [0, 1, 2]
+
 
 // INITIALIZE HERE//
 
@@ -357,6 +367,14 @@ function scroll_all(data) {
             heat_map(data, 'hopeless')
 
         }
+        else if (response.index == 2) {
+            timeOuts.forEach(function (timeOutFn) {
+                clearTimeout(timeOutFn);
+            });
+
+            heat_map(data, 'nervous')
+
+        }
 
     }
 
@@ -387,46 +405,161 @@ function scroll_all(data) {
     init();
 
     function heat_map(data, conditional) {
-        var margin = { top: 30, right: 30, bottom: 30, left: 30 },
-            width = 450 - margin.left - margin.right,
-            height = 450 - margin.top - margin.bottom;
+
+        //Define fixed variables
+        const height = 600;
+        const width = 600;
+        const margin = { left: 120, top: 50, bottom: 170, right: 70 };
+        const plotWidth = width - margin.left - margin.right;
+        const plotHeight = height - margin.top - margin.bottom;
+
 
         select('#app svg').remove()
+        select('#app div').remove()
+
+        //Initialize svg  
         var svg = select("#app")
             .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
+            .attr("width", width)
+            .attr("height", height)
             .append("g")
             .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
 
+        //Get data of interest
         const data_of_interest = data.filter(d => d.conditional_var == conditional)
 
+        //Define Fixed Groups for Heatmap
         var group_ages = unique(data_of_interest, 'demo_cat')
         var frequencies = unique(data_of_interest, 'conditional_cat').sort()
+
         // Build X scales and axis:
         var x = scaleBand()
-            .range([0, width])
+            .range([0, plotWidth])
             .domain(group_ages)
             .padding(0.01);
 
         svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(axisBottom(x))
+            .attr("transform", "translate(0," + plotHeight + ")")
+            .call(
+                axisBottom(x)
+                    .tickValues(group_ages)
+                    .tickFormat(row => demo_dic['age'][Number(row)])
+            )
+        //Title and Axis Labels
 
-        // Build X scales and axis:
+        //Other Fixed Aesthetics 
+        const title_dic = {
+            "rest_less": 'Restlessness',
+            'hopeless': 'Hopelessness', 'nervous': 'Nervousness'
+        }
+        svg.append("text")
+            .attr("x", plotWidth - 200)
+            .attr("y", -8 - (margin.top / 2))
+            .attr('id', 'title-first-graph')
+            .attr("text-anchor", "middle")
+            .style("font-size", "17px")
+            .style('font-weight', 'bold')
+            .style('fill', 'black')
+            .text(`Average Arrests`)
+            .attr('class', 'title')
+
+        svg.append("text")
+            .attr("x", plotWidth / 2 - 100)
+            .attr("y", plotHeight + 120)
+            .attr("text-anchor", "middle")
+            .style("font-size", "12px")
+            .text("Source: BJS 2016 Inmates Survey")
+            .attr('id', 'legend');
+
+        //Y and X labels
+        svg.append("text")
+            .attr('class', 'y-label')
+            .attr("transform", "rotate(-90)")
+            .attr("y", -1 - margin.left)
+            .attr("x", -plotHeight / 2)
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .style("font-size", "15px")
+            .style('font-weight', 'bold')
+            .style('fill', 'grey')
+            .text(`Frequency of ${title_dic[conditional]}`);
+
+        svg.append("text")
+            .attr('class', 'x-label')
+            .attr("x", plotWidth / 2)
+            .attr("y", plotHeight + 40)
+            .attr("dx", "1em")
+            .style("text-anchor", "middle")
+            .style("font-size", "15px")
+            .style('font-weight', 'bold')
+            .style('fill', 'grey')
+            .text('Age Group');
+
+        //Custom subtitle
+        svg.append("text")
+            .attr("x", plotWidth - 200)
+            .attr("y", 15 - (margin.top / 2))
+            .attr('id', 'subtitle-title-first-graph')
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style('font-weight', 'bold')
+            .style('fill', 'grey')
+            .text(`By Frequency of ${title_dic[conditional]} and Age`)
+            .attr('class', 'subtitle')
+
+
+
+        // Build Y scales and axis:
         var y = scaleBand()
-            .range([height, 0])
+            .range([plotHeight, 0])
             .domain(frequencies)
             .padding(0.01);
 
         svg.append("g")
-            .call(axisLeft(y));
+            .call(
+                axisLeft(y)
+                    .tickValues(frequencies)
+                    .tickFormat(row => emotional_dic[Number(row)])
+            );
+
 
         // Build color scale
         var color_scale = scaleSequential()
             .interpolator(interpolatePuBuGn)
             .domain(extent(data_of_interest, d => Number(d.times_arrested)))
+
+        //Transiton for every row in the heatmap
+        //Tooltip Section
+        // create a tooltip
+        var tooltip = select("#app")
+            .append("div")
+            .style("position", "absolute")
+            .attr('id', 'squares_tooltip')
+            .style("opacity", 0)
+            .attr("class", "squares_tooltip")
+
+        var formatDecimal = format(",.2f");
+        // Three function that change the tooltip when user hover / move / leave a cell
+        var mouseover = (event, d) => {
+            tooltip.style("opacity", 1)
+        }
+        var mousemove = (event, d) => {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 1)
+            console.log([event.pageX, event.pageY])
+            tooltip.html(
+                "<br/>" + '<b>' + 'Frequency: ' + '</b>' + emotional_dic[Number(d.conditional_cat)] +
+                "<br/>" + '<b>' + "Average Arrests: " + '</b>' + formatDecimal(d.times_arrested))
+                .style("left", (event.pageX / 3) + "px")
+                .style("top", (event.pageY / 12) + "px")
+                .style('background', 'white')
+        }
+        var mouseleave = (d) => {
+            tooltip.style("opacity", 0)
+        }
+
         let delay = 80
         frequencies.map(val => {
             timeOuts.push(setTimeout(function () {
@@ -435,23 +568,111 @@ function scroll_all(data) {
             delay += 500;
         })
         function squares_transition(val) {
-            svg.selectAll()
+            var squares = svg.selectAll()
                 .data(data_of_interest.filter(d => d.conditional_cat == val))
                 .enter()
                 .append("rect")
+                .attr('id', 'squares')
                 .merge(svg)
                 .attr("x", function (d) { return x(d.demo_cat) })
                 .attr("y", function (d) { return y(d.conditional_cat) })
                 .attr("width", x.bandwidth())
                 .attr("height", y.bandwidth())
                 .style('fill', 'white')
-                .transition()
+            squares.transition()
                 .duration(2000)
                 .style("fill", function (d) {
-                    //console.log(d.times_arrested)
                     return color_scale(Number(d.times_arrested))
                 })
+            squares.on("mouseover", mouseover)
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave)
         }
+
+        ////////////////////////////////////////////////////////////
+        ////////////////////////// Draw the legend /////////////////
+
+        //Calculate the variables for the temp gradient
+        var countScale = scaleLinear()
+            .domain(extent(data_of_interest, d => Number(d.times_arrested)))
+            .range([0, width])
+        var numStops = 10;
+        var countRange = countScale.domain()
+        countRange[2] = countRange[1] - countRange[0];
+
+        var countPoint = [];
+        for (var i = 0; i < numStops; i++) {
+            countPoint.push(i * countRange[2] / (numStops - 1) + countRange[0]);
+        }//for i
+        console.log(countPoint)
+        //Create the gradient
+        svg.append("defs")
+            .append("linearGradient")
+            .attr("id", "legend-recidivism")
+            .attr("x1", "0%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "0%")
+            .selectAll("stop")
+            .data(range(numStops))
+            .enter()
+            .append("stop")
+            .attr("offset", (d, idx) => {
+                return countScale(countPoint[idx]) / plotWidth;
+            })
+            .attr("stop-color", function (d, i) {
+                return color_scale(countPoint[i]);
+            });
+
+        var legendWidth = Math.min(width * 0.8, 400);
+        //Color Legend container
+        var legendsvg = svg.append("g")
+            .attr("class", "legendWrapper")
+            .attr("transform", `translate(${plotWidth / 2 + 100}, ${plotHeight + 65})`);
+
+        //Draw the Rectangle
+        legendsvg.append("rect")
+            .attr("class", "legendRect")
+            .attr("x", -legendWidth / 2)
+            .attr("y", 10)
+            //.attr("rx", hexRadius*1.25/2)
+            .attr("width", legendWidth / 2)
+            .attr("height", 10)
+            .style("fill", "url(#legend-recidivism)");
+
+        //Append title
+        legendsvg.append("text")
+            .attr("class", "legendTitle")
+            .attr("y", 0)
+            .attr("x", -legendWidth / 2 + 120)
+            .style("text-anchor", "middle")
+            .style('font-size', '12px')
+            .text("Average Number of Arrests");
+
+        //Set scale for x-axis
+        var legendDomain = extent(data_of_interest, d => Number(d.times_arrested))
+        var xScaleLegend = scaleLinear()
+            .range([-legendWidth / 2, 0])
+            .domain([legendDomain[0] - .3, legendDomain[1]])
+
+        //Define x-axis
+        var xAxisLegend = axisBottom(xScaleLegend)
+            .ticks(4)
+            .tickSizeOuter(0)
+            .tickSizeInner(0)
+
+
+        //Set up X axis
+        legendsvg.append("g")
+            .attr("class", "axis")
+            .attr('stroke-width', 0)
+            .attr('font-size', '10px')
+            .attr("transform", "translate(0," + (20) + ")")
+            .call(xAxisLegend)
+
+
+
+
 
     }
 }
